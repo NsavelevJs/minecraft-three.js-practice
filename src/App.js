a49e092599d584d53c669d5e37ccba631c6ac2c7
NsavelevJs <nsavelevjs@gmail.com>
2022-09-21 14:58:35 -0400

    added sky, ground and physics to environment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { Canvas } from '@react-three/fiber';
-import { Sky } from '@react-three/drei';
+import { Canvas } from "@react-three/fiber";
+import { Sky } from "@react-three/drei";
+import { Physics } from "@react-three/cannon";
+import { Ground } from "./components/Ground";
 
 function App() {
   return (
     <>
-      
       <Canvas>
-        <Sky sunPosition={[100,100,220]} />
+        <Sky sunPosition={[100, 100, 220]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Ground />
+        </Physics>
       </Canvas>
-
     </>
   );
 }
